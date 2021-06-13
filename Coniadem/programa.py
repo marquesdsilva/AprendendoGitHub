17df3a2cdb5c8a6941bd9cf0994b7b06a4e21858
Renato Marques <78667317+marquesdsilva@users.noreply.github.com>
2021-06-12 21:58:28 -0300

    Update programa.py

--- Coniadem/programa.py
+++ Coniadem/programa.py
@@ -1,11 +1,11 @@
 import mysql.connector
+from mysql.connector import cursor
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="828706",
+  password="senha",
   database= "marks_bazar"
 )
 mycursor = mydb.cursor()
-
 mycursor.execute("CREATE TABLE teste (nome VARCHAR(50), endereço VARCHAR(50))")
 
